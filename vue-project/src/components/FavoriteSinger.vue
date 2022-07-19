45dc990d43787ba05ed3da9cef8d6d43c3d873ff
LEEBEEE <101805395+LEEBEEE@users.noreply.github.com>
2022-07-19 12:43:00 +0900

    vue-first-project /연습

---
+++ vue-project/src/components/FavoriteSinger.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- <img v-bind:src="imgSrc"> -->
+    <img class="imgsize" :src="imgSrc">
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            imgSrc: 'https://img.vogue.co.kr/vogue/2021/12/style_61c299401f4b3-1200x801.jpeg'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .imgsize {width: 700px;}
+</style>
