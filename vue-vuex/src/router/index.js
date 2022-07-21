9ddc90444aa2a4b1fb75edeb259bc74136fc3442
LEEBEEE <101805395+LEEBEEE@users.noreply.github.com>
2022-07-21 17:23:22 +0900

    vue-vuex/연습

---
+++ vue-vuex/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import StoreAccess from '../views/StoreAccess.vue';
+import StoreAccess2 from '../views/StoreAccess2.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/storeAccess',
+    name: 'storeAccess',
+    component: StoreAccess,
+  },
+  {
+    path: '/storeAccess2',
+    name: 'storeAccess2',
+    component: StoreAccess2,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
